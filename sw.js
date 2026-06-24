e8a5e656714b52f93d717e0c59a72a1d719584d9
Koshikiko <pufferamr@gmail.com>
2026-06-24 15:48:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'myunipd-v1';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
